3197a1a537eb9a3522ad3fe538fdd826f9ad103b
DESKTOP-D54PBUI\19272 <1927236438@qq.com>
2024-08-01 17:51:27 +0800

    feat: Tooltip

---
+++ packages/components/Tooltip/index.ts
@@ -0,0 +1,6 @@
+import Tooltip from './Tooltip.vue'
+import {withInstall} from '@melon-element/utils'
+
+export const MelonTooltip = withInstall(Tooltip);
+
+export * from './types';
---
+++ packages/hooks/index.ts
@@ -0,0 +1,6 @@
+import useClickOutside from "./useClickOutside";
+import useEventListener from "./useEventListener";
+export {
+    useClickOutside,
+    useEventListener,
+}
