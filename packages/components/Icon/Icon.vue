0fda307a6566004cbe9347c89bd157a93639e35c
DESKTOP-D54PBUI\19272 <1927236438@qq.com>
2024-08-07 10:44:49 +0800

    fix: 解决按钮color不生效

--- packages/components/Icon/Icon.vue
+++ packages/components/Icon/Icon.vue
@@ -1,26 +1,28 @@
 <script setup lang="ts">
-import type {IconProps} from './types'
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
-import {omit} from "lodash-es"
-import {computed} from "vue"
+import type { IconProps } from "./types";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { omit } from "lodash-es";
+import { computed } from "vue";
 
 defineOptions({
-    name:'MelonIcon',
-    inheritAttrs:false
-})
-const props = defineProps<IconProps>()
-const filterProps = computed(()=>omit(props,["type","color"]))
-const customStyles = computed(()=>{color:props.color??void 0})
+  name: "MelonIcon",
+  inheritAttrs: false,
+});
+const props = defineProps<IconProps>();
+const filterProps = computed(() => omit(props, ["type", "color"]));
+const customStyles = computed(() => ({
+  color: props.color ?? void 0,
+}));
 </script>
 <template>
-    <i
-        class="melon-icon"
-        :class = "[`melon-icon-${props.type}`]"
-        :style="customStyles"
-        v-bind="$attrs"
-    >
-        <font-awesome-icon v-bind="filterProps"/>
-    </i>
+  <i
+    class="melon-icon"
+    :class="{ [`melon-icon--${type}`]: type }"
+    :style="customStyles"
+    v-bind="$attrs"
+  >
+    <font-awesome-icon v-bind="filterProps" />
+  </i>
 </template>
 <style scoped>
 .melon-icon {
